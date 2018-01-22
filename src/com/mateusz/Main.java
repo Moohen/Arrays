a3d88616f23712af9e490e19a124ab4ad63bf389
Moohen <mateusz.czekala.masazysta@gmail.com>
2018-01-22 22:48:48 +0100

    Initial commit

---
+++ src/com/mateusz/Main.java
@@ -0,0 +1,15 @@
+package com.mateusz;
+
+import java.util.Scanner;
+
+public class Main {
+
+    // The scanner allows you enter data into the console
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
